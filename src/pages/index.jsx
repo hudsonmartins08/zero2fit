98d4244555654785b8b1d78b55a40c77434a88a4
hudsonmartins08 <hudsonmartins108@gmail.com>
2025-05-14 10:10:47 -0300

    alteração de bg

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,15 @@
 export default function Home(){
   return(
-    <div className="w-full h-screen bg-red-500"></div>
+    <div className="w-[50%] h-screen bg-[#178080] flex items-start justify-center flex-col gap-6 pl-4">
+      <h1 className="w-[50%] text-[50px] text-white font-bold ">Comece aqui sua jornada fitness com a Zero2Fit</h1>
+      <h2 className="w-[50%] text-[20px] text-white font-semibold">Planos de treino personalizados, conselhos nutricionais e recursos de condicionamento físico para ajudá-lo a atingir seus objetivos de saúde.</h2>
+      <button className=" h-[45px] p-3 font-semibold justify-center flex text-white border outline-none rounded-md bg-[#FF7F6A] hover:bg-[#FF7F6A]/80">Comece sua jornada</button>
+      
+    </div>
+   
+    
+    
+    
   )
 }
   
